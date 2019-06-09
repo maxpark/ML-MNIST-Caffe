de96596a30dbbf4f55c13bc8abd4379c0c828fe4
Daniele Bagni <daniele.bagni@xilinx.com>
2019-06-09 16:58:44 +0200

    Create aws_activate_dnndk_cuda9.sh

---
+++ aws_scripts/aws_activate_dnndk_cuda9.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+source deactivate tensorflow_p27
+source deactivate caffe_p27
+export LD_LIBRARY_PATH=$HOME/ML/DNNDK/cuda9_tools/:/usr/local/cuda-8.0/lib64/:/$LD_LIBRARY_PATH
+export PATH=$HOME/ML/DNNDK/cuda9_tools/:$PATH
+
+#ln -s $HOME/ML/DNNDK/cuda9_tools/dnnc-dpu1.3.0 $HOME/ML/DNNDK/cuda9_tools/dnnc
+#ln -s $HOME/ML/DNNDK/cuda9_tools $HOME/ML/DNNDK/tools
