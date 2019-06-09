ea1c2bf72ff0aa2e3adb9c3d0ff25ac6edc43042
Daniele Bagni <daniele.bagni@xilinx.com>
2019-06-09 17:01:17 +0200

    Create dnnc_LeNet.sh

---
+++ deephi/LeNet/quantiz/dnnc_LeNet.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+ML_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && cd ../../.. && pwd )"
+export ML_DIR
+$(which dnnc &> /dev/null) || export PATH=$HOME/ML/DNNDK/tools:$PATH
+#DNNDK_ROOT=$HOME/ML/DNNDK/tools
+
+net=LeNet
+work_dir=$ML_DIR/deephi/LeNet/quantiz
+model_dir=${work_dir}/decent_output
+output_dir=${work_dir}/dnnc_output
+
+echo "Compiling network: ${net}"
+
+dnnc --prototxt=${model_dir}/deploy.prototxt     \
+       --caffemodel=${model_dir}/deploy.caffemodel \
+       --output_dir=${output_dir}                  \
+       --net_name=${net}                           \
+       --dpu=4096FA                                \
+       --cpu_arch=arm64                            \
+       --mode=debug                                \
+       --save_kernel
+
+
+
+echo " copying dpu elf file into /../zcu102/baseline/model/arm64_4096 "
+cp ${output_dir}/dpu_${net}\_*.elf  ${output_dir}/../../zcu102/baseline/model/arm64_4096
+
+echo " copying the test images to be used by the ZCU102"
+cp $ML_DIR/input/mnist_jpg/test/* ${output_dir}/../../zcu102/test_images
+
+#mv ${net}_kernel*.* ${work_dir}/dnnc_output
+
+# The following lines are for documentation only. Leave them commented
+# now you can create the tar archive that you will then scp/ssh copy from HOST PC to TARGET board 
+## cd ~/ML/cifar10/deephi/miniVggNet/zcu102/baseline
+## ln -s ../test_images ./test_images
+## cd ..
+## tar -xvf baseline.tar ./baseline
+
+
+
+
+
